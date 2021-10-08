61d72fca694b4efad2a5253c3b5ed028ddeeff23
jerome-karabenli <jkarabenli.oclock@gmail.com>
2021-10-09 00:43:49 +0200

    jfidjié

--- docker/backup-moving.sh
+++ docker/backup-moving.sh
@@ -4,7 +4,7 @@ echo "db dump in progress ..."
 docker exec -i ochalet_postgres sh "/db_dump.sh"
 
 echo "sending dump for backup..."
-rsync --delete -avrhe "ssh -p 26" $BASEDIR/ pi@192.168.1.10:/home/pi/test/
+rsync --delete -avrhe "ssh -p 5000" $BASEDIR/ pi@rpiweb:/home/pi/test/
 echo "dump sent on backup server !"
 
 echo "deleting files older than 3 days..."
--- docker/container-laucher.sh
+++ docker/container-laucher.sh
@@ -4,9 +4,15 @@ sleep 1
 docker exec -it ochalet_api sh "migrations/sqitch-init.sh" 
 docker cp ./db_dump.sh ochalet_postgres:/db_dump.sh
 
+crontab -l > db_dump_cron
+echo "*/1 * * * * /bin/sh /root/linode-test/docker/backup-moving.sh >> /root/linode-test/docker/backup-moving.log 2>&1"
+crontab db_dump_cron
+rm db_dump_cron
 
-#Autoriser les connexions entrantes pour ces ports
-# iptables -A INPUT -p tcp --dport 3000 -j ACCEPT #dev-talk api dev
-#Autoriser les connexions sortantes pour ces ports
-# iptables -A OUTPUT -p tcp --dport 3000 -j ACCEPT #dev-talk api dev
+
+
+Autoriser les connexions entrantes pour ces ports
+iptables -A INPUT -p tcp --dport 3000 -j ACCEPT 
+Autoriser les connexions sortantes pour ces ports
+iptables -A OUTPUT -p tcp --dport 3000 -j ACCEPT
 
