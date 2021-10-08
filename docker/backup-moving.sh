a0855d1cf7355fb7036fbf650190651bfb0ab372
jerome-karabenli <jkarabenli.oclock@gmail.com>
2021-10-09 01:00:47 +0200

    jdd

--- docker/backup-moving.sh
+++ docker/backup-moving.sh
@@ -4,7 +4,7 @@ echo "db dump in progress ..."
 docker exec -i ochalet_postgres sh "/db_dump.sh"
 
 echo "sending dump for backup..."
-rsync --delete -avrhe "ssh -p 5000" $BASEDIR/ pi@rpiweb.hopto.org:/home/pi/test/
+rsync --delete -avrhe "ssh -p 5000" /root/linode-test/db-dump/ pi@rpiweb.hopto.org:/home/pi/test/
 echo "dump sent on backup server !"
 
 echo "deleting files older than 3 days..."
