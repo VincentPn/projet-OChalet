4222588d483e9e9406c6058c9268cdb0c46e8e2b
jerome-karabenli <jkarabenli.oclock@gmail.com>
2021-10-09 00:27:59 +0200

    test

--- api/migrations/sqitch-init.sh
+++ api/migrations/sqitch-init.sh
@@ -1,4 +1,7 @@
-sqitch init ochalet --target db:pg://ochalet:ochalet@ochalet_postgres:5432/ochalet
-sleep 2
+sqitch init ochalet --target db:pg://ochalet:ochalet@ochalet_postgres:5432/ochalet --top-dir ./migrations/
 sqitch deploy
-psql postgres://ochalet:ochalet@ochalet_postgres:5432/ochalet -f seeding/seeding.sql
+psql postgres://ochalet:ochalet@ochalet_postgres:5432/ochalet -f migrations/seeding/seeding.sql
+
+
+
+
