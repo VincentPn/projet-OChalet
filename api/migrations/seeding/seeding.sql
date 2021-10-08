3b6a7e9c837ebf4c24a7e758f165cabf3671d605
jerome-karabenli <jkarabenli.oclock@gmail.com>
2021-10-08 15:47:42 +0200

    jij

---
+++ api/migrations/seeding/seeding.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+INSERT INTO "location" ("name", "picture") VALUES
+('Alpes du Nord', 'https://images.unsplash.com/photo-1597216394928-e6bc38109801?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2338&q=80'),
+('Alpes du Sud', 'https://images.unsplash.com/photo-1594629706083-67fd4ea4f0d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2340&q=80'),
+('Jura', 'https://images.unsplash.com/photo-1603445125995-6f47a1bfccf0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2340&q=80'),
+('Massif Central', 'https://images.unsplash.com/photo-1603121494413-0d31fe0bfb1e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2340&q=80'),
+('Pyrennées', 'https://images.unsplash.com/photo-1538427144912-31de1ccb39f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'),
+('Vosges', 'https://images.unsplash.com/photo-1604782101560-b1cf32c445a8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2340&q=80');
+
+COMMIT;
--- api/migrations/sqitch-init.sh
+++ api/migrations/sqitch-init.sh
@@ -1,3 +1,4 @@
 sqitch init ochalet --target db:pg://ochalet:ochalet@ochalet_postgres:5432/ochalet
 sleep 2
 sqitch deploy
+psql postgres://ochalet:ochalet@ochalet_postgres:5432/ochalet -f seeding/seeding.sql
