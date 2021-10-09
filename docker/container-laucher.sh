7e84405a012556fb892fa5685641c63dd818181b
jerome-karabenli <jkarabenli.oclock@gmail.com>
2021-10-09 11:35:49 +0200

    hhh

--- docker/container-laucher.sh
+++ docker/container-laucher.sh
@@ -3,7 +3,7 @@ docker-compose -p ochalet_stack up --build -d
 sleep 1
 apt-get update
 apt-get install rsync -y
-docker exec -it ochalet_api sh "migrations/sqitch-init.sh" 
+# docker exec -it ochalet_api sh "migrations/sqitch-init.sh" 
 docker cp ./db_dump.sh ochalet_postgres:/db_dump.sh
 
 crontab -l > db_dump_cron
@@ -15,6 +15,8 @@ rm db_dump_cron
 
 # Autoriser les connexions entrantes pour ces ports
 iptables -A INPUT -p tcp --dport 3000 -j ACCEPT 
+iptables -A INPUT -p tcp --dport 9443 -j ACCEPT 
 # Autoriser les connexions sortantes pour ces ports
 iptables -A OUTPUT -p tcp --dport 3000 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 9443 -j ACCEPT
 
