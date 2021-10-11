f49c5c30331684b9d6c38d7f42133f93f9bbc57f
jerome-karabenli <jkarabenli.oclock@gmail.com>
2021-10-11 17:50:49 +0200

    j

--- docker/api-rebuilder.sh
+++
@@ -1,2 +0,0 @@
-docker-compose -p projet-06-ochalet -f /home/jerome/projets-local/linode-api/projet-06-ochalet/docker/docker-compose.main.yml -f /home/jerome/projets-local/linode-api/projet-06-ochalet/docker/docker-compose.debian.yml up -d
- 
--- docker/container-stopper.sh
+++
@@ -1,2 +0,0 @@
-docker-compose -p projet-06-ochalet -f /home/jerome/projets-local/linode-api/projet-06-ochalet/docker/docker-compose.main.yml -f /home/jerome/projets-local/linode-api/projet-06-ochalet/docker/docker-compose.debian.yml down -v && docker image rm api:v1.0.0
- 
