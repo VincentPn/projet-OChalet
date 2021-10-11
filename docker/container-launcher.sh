cf0d38726efd198424620776c093ecbeff17752c
jerome-karabenli <jkarabenli.oclock@gmail.com>
2021-10-11 10:43:35 +0200

    kk

--- docker/container-launcher.sh
+++ docker/container-launcher.sh
@@ -11,8 +11,8 @@ DB_PASSWORD="bingofjdifjidjfdi"
 DB_PORT="5432"
 DB_NAME="jfidjijdfidfj"
 
-PATH_TO_REPO="/home/jerome/projets-local/APO2/"
-REPO_NAME="projet-06-ochalet"
+PATH_TO_REPO="/root/"
+REPO_NAME="linode-test"
 
 PATH_TO_MAIN_COMPOSE_FILE="$PATH_TO_REPO$REPO_NAME/docker/docker-compose.main.yml"
 PATH_TO_DEBIAN_COMPOSE_FILE="$PATH_TO_REPO$REPO_NAME/docker/docker-compose.debian.yml"
