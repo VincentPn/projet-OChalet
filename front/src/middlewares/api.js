553cc888dbdd1efe0e71ed8d81edea2fc5295579
jerome-karabenli <jkarabenli.oclock@gmail.com>
2021-10-14 13:46:10 +0200

    jjj

--- front/src/middlewares/api.js
+++ front/src/middlewares/api.js
@@ -32,7 +32,7 @@ import {
 
 const axiosInstance = axios.create({
   // baseURL: 'http://ochaleto.ddns.net',
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://api:3000',
   // baseURL: 'http://localhost:5000',
 });
 
